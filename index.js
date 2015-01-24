710766db996f4dcbd309ea36a00daea427051ee8
Niranjan_Bala <niranjan.veerasekaran@commonfloor.com>
2015-01-24 15:27:34 +0530

    springrole

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var app = express();
+/*var passport = require('passport'),
+    SpringRoleStrategy = require('passport-springrole').Strategy;
 
+passport.use(new SpringRoleStrategy({
+    clientID: '8d79eedc',
+    callbackURL: "https://science-hack.herokuapp.com/"
+  },
+  function(accessToken, refreshToken, profile, done) {
+  	console.log(accessToken, refreshToken.)
+  }
+));*/
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
-
+app.get('/auth/springrole/callback', function(request, response) {
+  response.send('Hello World!')
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
